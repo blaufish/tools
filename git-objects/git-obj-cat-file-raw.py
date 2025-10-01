6e005a23989c077413ad070072d9cb912f0fa557
blaufish <blaufish@users.noreply.github.com>
2025-10-01 14:44:25 +0200

    enable binary dump

--- git-objects/git-obj-cat-file-raw.py
+++ git-objects/git-obj-cat-file-raw.py
@@ -10,7 +10,6 @@ import zlib
 def read_file(filename, debug_bin, debug_sha):
     compressed_contents = open(filename, 'rb').read()
     decompressed_contents = zlib.decompress(compressed_contents)
-    utf8 = decompressed_contents.decode("utf-8")
 
     if debug_bin:
         print(decompressed_contents, file=sys.stderr)
@@ -19,7 +18,14 @@ def read_file(filename, debug_bin, debug_sha):
         sha1 = hashlib.sha1(decompressed_contents).hexdigest()
         print(f"SHA1: {sha1}", file=sys.stderr)
 
-    return utf8
+    return decompressed_contents
+
+def output(object_bin, decode):
+    if decode is None:
+        sys.stdout.buffer.write(object_bin)
+    else:
+        object_str = object_bin.decode(decode)
+        print(f"{object_str}", end="")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -29,10 +35,11 @@ def main():
     parser.add_argument('file')
     parser.add_argument('--debug-bin', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('--debug-sha', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('--decode', default=None)
     args = parser.parse_args()
 
-    object_str = read_file(args.file, args.debug_bin, args.debug_sha)
-    print(f"{object_str}", end="")
+    object_bin = read_file(args.file, args.debug_bin, args.debug_sha)
+    output(object_bin, args.decode)
 
 if __name__ == "__main__":
     main()
